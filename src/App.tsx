b0879bc3f465ec35e50615b85e5cf7d924c0559c
Lars Karbo <mail@larskarbo.no>
2020-11-04 11:00:20 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import img from "./img.png";
+import lars from "./lars.png";
+import julie from "./julie.png";
 import "./App.css";
 
 function App() {
@@ -20,59 +22,122 @@ function App() {
           margin: 0,
           padding: "0px 25px",
           color: "white",
-          fontFamily: "Roboto Mono, Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
-          fontWeight: 300
+          fontFamily:
+            "Roboto Mono, Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
+          fontWeight: 300,
         }}
       >
-        <div style={{padding: 50}}></div>
-        <h1 style={{
-          fontWeight: 600,
-          // textTransform: "uppercase",
-          // fontFamily: "-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif"
-        }}>Rise Age</h1>
+        <div style={{ padding: 50 }}></div>
+        <h1
+          style={{
+            fontWeight: 600,
+            // textTransform: "uppercase",
+            // fontFamily: "-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif"
+          }}
+        >
+          Rise Age
+        </h1>
         <p>
-          New <strong>performance oriented</strong> clinic coming soon to Oslo. Helping
-          individuals reach their maximum potential through <strong>mental training</strong>.
+          New <strong>performance oriented</strong> clinic coming soon to Oslo.
+          Helping individuals reach their maximum potential through{" "}
+          <strong>mental training</strong>.
         </p>
 
-        <hr style={{
-          width: "100%"
-        }} />
+        <hr
+          style={{
+            width: "100%",
+          }}
+        />
         <p>Nøkkelord: mental trening, røykeslutt, selvbilde, søvn.</p>
         <p>Ta kontakt med oss: mail@riseage.no</p>
 
-        <div style={{padding: 10}}></div>
+        <div style={{ padding: 10 }}></div>
 
-        <img src={img} style={{
-          width:"100%",
-          maxWidth:400,
-          height: "auto"
-        }} />
-        <div style={{padding: 20}}></div>
-
-        <div style={{
-          display: "flex",
-          justifyContent: "center"
-        }} className="anim">
-          <div style={{
-            width: 50,
-            height: 50,
-            borderRadius: 50,
-            background: "#C27500",
-            position: "relative",
-            top: 30,
-            zIndex: -1
-          }}></div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+            className="bio"
+          >
+            <img
+              src={julie}
+              style={{
+                width: "100%",
+                maxWidth: 200,
+                height: "auto",
+              }}
+            />
+            <div>
+              <strong>Julie Kamp</strong>
+            </div>
+            <div>Kommende hypnoterapeut</div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+            className="bio"
+          >
+            <img
+              src={lars}
+              style={{
+                width: "100%",
+                maxWidth: 200,
+                height: "auto",
+              }}
+            />
+            <div>
+              <strong>Lars Karbø</strong>
+            </div>
+            <div>Kommende hypnoterapeut</div>
+          </div>
         </div>
-        <hr style={{
-          margin: 0,
-          width: "100%", zIndex: 10
-        }} />
-        <div style={{paddingBottom: 50, paddingTop: 5, background: "black", zIndex: 10}}>
-        <p>© 2020 Rise Age - All rights reserved</p>
-        </div>
-        
+        <div style={{ padding: 20 }}></div>
 
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+          className="anim"
+        >
+          <div
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 50,
+              background: "#C27500",
+              position: "relative",
+              top: 30,
+              zIndex: -1,
+            }}
+          ></div>
+        </div>
+        <hr
+          style={{
+            margin: 0,
+            width: "100%",
+            zIndex: 10,
+          }}
+        />
+        <div
+          style={{
+            paddingBottom: 50,
+            paddingTop: 5,
+            background: "black",
+            zIndex: 10,
+          }}
+        >
+          <p>© 2020 Rise Age - All rights reserved</p>
+        </div>
       </div>
     </div>
   );
